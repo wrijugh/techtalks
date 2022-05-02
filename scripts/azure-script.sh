a8abbfabe47895124b0da30f8c238cb9574ccde9
Wriju - Personal <49710438+wrijugh@users.noreply.github.com>
2022-05-02 22:30:23 +0530

    s

--- scripts/azure-script.sh
+++ scripts/azure-script.sh
@@ -1,4 +1,5 @@
 # Global Azure May 2022
+
 rnd=$RANDOM
 g=rg-demo$rnd
 
@@ -8,37 +9,43 @@ az group create -n $g -l eastus
 acr=wgacr$rnd
 az acr create -g $g -n $acr --admin-enabled=true --sku=basic
 
-echo "Please copy the ACR password and paste here:"
-# -----------
-acrpwd=XjUfuyeuBF8L4r8DDRleYbdh/Gi3Vd1y
+# -----------Manually Copy the Password from Portal------------
+acrpwd=BieaKhEMFHBrDougN9tl8xAh3Z9XV/iR
 
 docker login -u $acr -p $acrpwd $acr.azurecr.io
 
 az acr login -n $acr
 
 ## Download the nginx image to local machine 
+img=nginx
+docker pull $img
 
-docker pull nginx
-
-image=$acr.azurecr.io/nginx:latest
+image=$acr.azurecr.io/$img:latest
 
 docker images
+#----------------Manually copy the IMAGE ID from console-----------
+# Task: Get docker image ID for $img
 image_id=fa5269854a5e
 
 docker tag $image_id $image
 
 docker push $image
 
-# ---------------
 # Azure Container Instance (ACI)
 aci=nginx$rnd
 az container create -g $g -n $aci --image=$image --ip-address=Public --registry-password=$acrpwd --registry-username=$acr 
 
-## Azure App Service (Container)
+# Task: Get Public IP
+
+
+# Azure App Service (Container)
 plan=asPlan$rnd
 webapp=wgwebapp$rnd
+
 az appservice plan create -g $g -n $plan --is-linux --sku B2
 
-az webapp create -g $g -n $webapp -p $plan -i=$image -s wgacr -w $acrpwd
+az webapp create -g $g -n $webapp -p $plan -i=$image -s $acr -w $acrpwd
+
+curl http://$webapp.azurewebsites.net 
 
 # Azure Kubernetes Service (AKS)
