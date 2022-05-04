3f35a4586e51a242dc24fdbe37a1d391bda10e7a
Wriju - Personal <49710438+wrijugh@users.noreply.github.com>
2022-05-04 13:10:06 +0530

    echo

--- scripts/01-azure-ubuntu-vm.sh
+++ scripts/01-azure-ubuntu-vm.sh
@@ -7,6 +7,8 @@ loc=southeastasia
 adminuser='wriju'
 pwd='P@ssw0rd1234'
 
+echo 'VM=$vm will be created inside Resource Group:$g'
+
 az group create -n $g -l $loc
 
 # Economic VM
@@ -16,5 +18,4 @@ az vm create -n $vm -g $g --image=ubuntults --generate-ssh-keys \
 az vm auto-shutdown -g $g -n $vm --time 1430
 
 
-az vm create -g $g -n $vm --image=ubuntults --generate-ssh-keys --use-unmanaged-disk --storage-sku=Standard_LRS --size Standard 
-    --authentication-mode=all
+az vm create -g $g -n $vm --image=ubuntults --generate-ssh-keys 
