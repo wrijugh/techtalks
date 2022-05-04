ea2fe1a56d15de52d8c9db1d52ee3f7ab55c4f13
Wriju - Personal <49710438+wrijugh@users.noreply.github.com>
2022-05-04 14:30:19 +0530

    almost

--- scripts/01-azure-ubuntu-vm.sh
+++ scripts/01-azure-ubuntu-vm.sh
@@ -20,6 +20,8 @@ az vm auto-shutdown -g $g -n $vm --time 1430
 # to get the Public IP
 az vm show -d -g $g -n $vm --query 'publicIps' -o tsv 
 
+ssh $adminuser@$(az vm show -d -g $g -n $vm --query 'publicIps' -o tsv)
+
 # To update VM with new SSH key
 ssh-keygen 
 
